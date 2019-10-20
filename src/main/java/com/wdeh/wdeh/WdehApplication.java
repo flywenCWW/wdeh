7c0517594b67a493d638c724fb68bcdede89d8ad
chenwenwei <chenwenweiwj65424@touna.con>
2019-10-20 21:52:19 +0800

    Initial commit

---
+++ src/main/java/com/wdeh/wdeh/WdehApplication.java
@@ -0,0 +1,12 @@
+package com.wdeh.wdeh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WdehApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WdehApplication.class, args);
+    }
+}
---
+++ src/test/java/com/wdeh/wdeh/WdehApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wdeh.wdeh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WdehApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
